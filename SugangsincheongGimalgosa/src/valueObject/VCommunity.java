79f8316cd888bd045d28ec98b690a5c7ba1ff7ea
pc <pc@DESKTOP-SSVNIKK>
2023-06-11 17:44:51 +0900

    수정

---
+++ SugangsincheongGimalgosa/src/valueObject/VCommunity.java
@@ -0,0 +1,34 @@
+package valueObject;
+
+public class VCommunity {
+	private String title;
+	private String content;
+	private String name;
+	private int code;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	
+}
